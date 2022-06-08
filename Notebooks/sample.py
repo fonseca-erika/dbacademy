adbc9880c9d7901b529494dcb5911cb3af8d63c3
fonseca.erika@gmail.com <fonseca.erika@gmail.com>
2022-06-08 16:58:07 +0000

    Synchronising to GitHub

---
+++ Notebooks/sample.py
@@ -0,0 +1,12 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC 
+# MAGIC # Sample Notebook to test integration with github
+
+# COMMAND ----------
+
+print('Hello world!')
+
+# COMMAND ----------
+
+
